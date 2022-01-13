536077cdabaabbe48b788afda3bdc954ad6d1cf4
KangSuji <sukang0608@naver.com>
2022-01-13 11:03:04 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'SUJI',
+    email: 'sukang0608@naver.com',
+    blog: 'https://creambbang0608.tistory.com/',
+    phone: '+82-010-9660-5987',
+    image: './src/assets/logo.png'
+  })
+}
